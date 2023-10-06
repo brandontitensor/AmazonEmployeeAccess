b6729b1a9e630f61b5c0d4abfcfb88ddf719970f
brandontitensor <97186556+brandontitensor@users.noreply.github.com>
2023-10-06 13:54:57 -0600

    Added Modeling code

---
+++ AmazonModeling.R
@@ -0,0 +1,64 @@
+#############
+##LIBRARIES##
+#############
+
+library(tidymodels) #For the recipes
+library(tidyverse) #Given for EDA
+library(poissonreg) #For Poisson Regression
+library(vroom) #For reading in data
+library(DataExplorer)
+library(glmnet)
+library(mltools)
+library(randomForest)
+library(doParallel)
+library(xgboost)
+tidymodels_prefer()
+conflicted::conflicts_prefer(yardstick::rmse)
+library(rpart)
+library(stacks) #For stacking
+library(ggmosaic)
+
+
+####################
+##WORK IN PARALLEL##
+####################
+
+all_cores <- parallel::detectCores(logical = FALSE)
+registerDoParallel(cores = all_cores)
+
+########
+##DATA##
+########
+
+my_data <- vroom("train.csv")
+test_data <- vroom("test.csv")
+
+#######
+##EDA##
+#######
+
+DataExplorer::plot_histogram(my_data)
+GGally::ggpairs(my_data)
+
+plot_1 <- ggplot(data=my_data) +
+  geom_mosaic(aes(x=product(RESOURCE), fill = ACTION))
+
+##########
+##RECIPE##
+##########
+
+my_recipe <- recipe(ACTION~., data=my_data) %>%
+step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
+  step_other(all_nominal_predictors(), threshold = .01) %>% # combines categorical values that occur <5% into an "other" value6
+  step_dummy(all_nominal_predictors()) 
+
+prepped_recipe <- prep(my_recipe, verbose = T)
+bake_1 <- bake(prepped_recipe, new_data = NULL)
+
+
+
+
+
+
+
+
